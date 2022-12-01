fc2aff3da0ecc32249d2cd9a3cae6bba7fe0b92d
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-01 06:09:20 +0100

    level1 part1 and 2

--- level1/Program.cs
+++ level1/Program.cs
@@ -1,3 +1,4 @@
-﻿string input = File.ReadAllText("sample.in");
+﻿List<List<int>> input = File.ReadAllText("level1.in").Split("\r\n\r\n").Select(x => x.Split("\r\n").Select(int.Parse).ToList()).ToList();
 
-
+Console.WriteLine(input.OrderByDescending(t => t.Sum()).FirstOrDefault().Sum());
+Console.WriteLine(input.OrderByDescending(t => t.Sum()).Take(3).Sum(x => x.Sum()));
