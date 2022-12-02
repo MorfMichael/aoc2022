ff1e222de8b744958696b152df132c2f91e635a0
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-02 06:18:50 +0100

    level2 part1

--- level2/Program.cs
+++ level2/Program.cs
@@ -1,3 +1,45 @@
-﻿string input = File.ReadAllText("level2.in");
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
-Console.WriteLine(input);
+string[] input = File.ReadAllLines("level2.in");
+
+int score = 0;
+
+List<(string a, string b, int score)> test = new()
+{
+    ("A", "X", 4),
+    ("B", "Y", 5) ,
+    ("C", "Z", 6),
+    ("A", "Z", 3),
+    ("B", "X", 1),
+    ("C", "Y", 2),
+    ("A", "Y", 8),
+    ("B", "Z", 9),
+    ("C", "X", 7),
+};
+
+foreach (var line in input)
+{
+    var split = line.Split(" ");
+    var a = split[0];
+    var b = split[1];
+
+    int s = test.FirstOrDefault(x => x.a == a && x.b == b).score;
+
+    if (s == 0)
+    {
+        Console.WriteLine("wrong");
+    }
+    score += s;
+}
+
+Console.WriteLine(score);
+
+// A = Rock
+// B = Paper
+// C = Siccors
+
+//X = Rock 1
+// Y = Paper 2
+// Z = Siccsors 3
+
