4034867071f5be8cc67feb61a53247faaffdc695
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-03 08:09:20 +0100

    level3 part1

---
+++ level3/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] input = File.ReadAllLines("level3.in");
+
+
+List<char> shared = new();
+
+foreach (var line in input)
+{
+    int split = line.Length / 2;
+    string left = line[..split];
+    string right = line[split..];
+
+    var s = left.Where(right.Contains).Distinct().ToList();
+    shared.AddRange(s);
+}
+
+int count = 0;
+foreach (var c in shared)
+{
+    int value = char.IsLower(c) ? (int)c - 96 : (int)c - 64 + 26;
+    Console.WriteLine($"{c}: {value}");
+    count += value;
+}
+
+Console.WriteLine(count);
