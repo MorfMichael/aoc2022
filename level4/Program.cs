e2786edb2c88f6a0a76a29df694dc3840981f4fa
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-04 06:15:18 +0100

    level4 part1

--- level4/Program.cs
+++ level4/Program.cs
@@ -1,6 +1,14 @@
 ﻿string[] input = File.ReadAllLines("level4.in");
 
+int count = 0;
 foreach (var line in input)
 {
-    Console.WriteLine(line);
+    var split = line.Split(",");
+    var left = split[0].Split("-").Select(int.Parse).ToArray();
+    var right = split[1].Split("-").Select(int.Parse).ToArray();
+
+    if (left[0] >= right[0] && left[1] <= right[1]) count++;
+    else if (right[0] >= left[0] && right[1] <= left[1]) count++;
 }
+
+Console.WriteLine(count);
