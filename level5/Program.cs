e34ebb0d821d968419c36afc1c9a6be6ae512c94
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-04 11:40:10 +0100

    level5 init

---
+++ level5/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] lines = File.ReadAllLines("level5.in");
+
+int count = 0;
+
+for (int i = 0; i < lines.Length; i++)
+{
+    string line = lines[i];
+
+    if (string.IsNullOrWhiteSpace(line)) continue;
+
+    /* write your code here */
+}
+
+Console.WriteLine(count);
