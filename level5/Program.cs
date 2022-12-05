42f89f4e1e73868d945ac5f894b4ee1f3cbd4f89
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-05 06:50:14 +0100

    level5 part1 and part2

--- level5/Program.cs
+++ level5/Program.cs
@@ -1,14 +1,40 @@
-﻿string[] lines = File.ReadAllLines("level5.in");
+﻿string[] lines = File.ReadAllText("level5.in").Split("\r\n\r\n");
 
-int count = 0;
+string[] instructions = lines[1].Split("\r\n");
 
-for (int i = 0; i < lines.Length; i++)
+string[] m = new[]
 {
-    string line = lines[i];
+    "BVSNTCHQ",
+"WDBG",
+"FWRTSQB",
+"LGWSZJDN",
+"MPDVF",
+"FWJ",
+"LNQBJV",
+"GTRCJQSN",
+"JSQCWDM",
+};
+
+
+for (int i = 0; i < instructions.Length; i++)
+{
+    string line = instructions[i];
 
     if (string.IsNullOrWhiteSpace(line)) continue;
+    int[] options = line.Replace("move ", "").Replace(" from ", ",").Replace(" to ", ",").Split(",").Select(int.Parse).ToArray();
+    //move 3 from 6 to 2
 
-    /* write your code here */
+    int source = options[1] - 1, target = options[2] - 1, count = options[0];
+    //part1
+    //for (int j = 0; j < count; j++)
+    //{
+    //    m[target] += m[source][^1];
+    //    m[source] = m[source][..^1];
+    //}
+
+    //part2
+    m[target] += m[source][^count..];
+    m[source] = m[source][..^count];
 }
 
-Console.WriteLine(count);
+Console.WriteLine(string.Join("", m.Select(t => t[^1])));
