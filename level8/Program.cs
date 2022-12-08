5a342715f90b580606b3110f3d2d1d4806b3a27a
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-08 06:21:35 +0100

    level8 part1

--- level8/Program.cs
+++ level8/Program.cs
@@ -5,13 +5,23 @@ int count = 0;
 for (int i = 0; i < lines.Length; i++)
 {
     string line = lines[i];
-
     if (string.IsNullOrWhiteSpace(line)) continue;
 
-    /* write your code here */
+    for (int j = 0; j < line.Length; j++)
+    {
+        int tree = int.Parse(line[j].ToString());
 
-    count++;
+        if (i == 0 || j == 0 || i == lines.Length - 1 || j == line.Length - 1) count++;
+        else
+        {
+            var left = line[0..j].Select(x => int.Parse(x.ToString())).ToArray();
+            var right = line[(j+1)..].Select(x => int.Parse(x.ToString())).ToArray();
+            var top = lines[0..i].Select(x => int.Parse(x[j].ToString())).ToArray();
+            var bottom = lines[(i+1)..].Select(x => int.Parse(x[j].ToString())).ToArray();
+
+            if (left.All(x => x < tree) || right.All(x => x < tree) || top.All(x => x < tree) || bottom.All(x => x < tree)) count++;
+        }
+    }
 }
-
 
 Console.WriteLine(count);
