080e55ad1e6a8c26cfbdf444f4ab2e81a0244d09
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-09 07:26:10 +0100

    level9 part2

--- level9/Program.cs
+++ level9/Program.cs
@@ -2,16 +2,144 @@
 
 int count = 0;
 
+Rope rope = new Rope();
+
 for (int i = 0; i < lines.Length; i++)
 {
     string line = lines[i];
 
     if (string.IsNullOrWhiteSpace(line)) continue;
 
-    /* write your code here */
-
-    count++;
+    var split = line.Split();
+    rope.Move(split[0], int.Parse(split[1]));
 }
 
+Console.WriteLine(rope.Result);
 
-Console.WriteLine(count);
+class Rope
+{
+    private List<(int X, int Y)> _tail = new();
+    private (int X, int Y) _start;
+
+    public Rope()
+    {
+        _start = (0, 0);
+        Head = new Knot(0, 0, 0);
+        Tails = Enumerable.Range(1, 9).Select(t => new Knot(t, 0, 0)).ToList();
+        _tail.Add((0, 0));
+    }
+
+    public List<Knot> Tails { get; set; }
+    public Knot Head { get; set; }
+
+    public int Result => _tail.Count;
+
+    public void Move(string direction, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            OneStep(direction);
+        }
+    }
+
+    public void OneStep(string direction)
+    {
+        switch (direction)
+        {
+            case "L":
+                Head.X--;
+                break;
+            case "R":
+                Head.X++;
+                break;
+            case "U":
+                Head.Y--;
+                break;
+            case "D":
+                Head.Y++;
+                break;
+        }
+
+        var head = Head;
+        foreach (var tail in Tails.OrderBy(t => t.Id))
+        {
+            MoveTail(tail, head);
+            head = tail;
+        }
+    }
+
+    public void MoveTail(Knot Tail, Knot Head)
+    {
+        if (Head.X >= Tail.X - 1 && Head.X <= Tail.X + 1 && Head.Y >= Tail.Y - 1 && Head.Y <= Tail.Y + 1) return;
+
+        if ((Head.X == Tail.X + 1 && Head.Y == Tail.Y - 2) ||
+            (Head.X == Tail.X + 2 && Head.Y == Tail.Y - 1) ||
+            (Head.X == Tail.X + 2 && Head.Y == Tail.Y - 2))
+        {
+            Tail.X++;
+            Tail.Y--;
+        }
+
+        if (Head.X == Tail.X + 2 && Head.Y == Tail.Y)
+            Tail.X++;
+
+        if ((Head.X == Tail.X + 2 && Head.Y == Tail.Y + 1) ||
+            (Head.X == Tail.X + 1 && Head.Y == Tail.Y + 2) ||
+            (Head.X == Tail.X + 2 && Head.Y == Tail.Y + 2))
+        {
+            Tail.X++;
+            Tail.Y++;
+        }
+
+        if (Head.X == Tail.X && Head.Y == Tail.Y + 2)
+        {
+            Tail.Y++;
+        }
+
+        if ((Head.X == Tail.X - 1 && Head.Y == Tail.Y + 2) ||
+            (Head.X == Tail.X - 2 && Head.Y == Tail.Y + 1) ||
+            (Head.X == Tail.X - 2 && Head.Y == Tail.Y + 2))
+        {
+            Tail.X--;
+            Tail.Y++;
+        }
+
+        if (Head.X == Tail.X - 2 && Head.Y == Tail.Y)
+        {
+            Tail.X--;
+        }
+
+        if ((Head.X == Tail.X - 2 && Head.Y == Tail.Y - 1) ||
+            (Head.X == Tail.X - 1 && Head.Y == Tail.Y - 2) ||
+            (Head.X == Tail.X - 2 && Head.Y == Tail.Y - 2))
+        {
+            Tail.X--;
+            Tail.Y--;
+        }
+
+        if (Head.X == Tail.X && Head.Y == Tail.Y - 2)
+        {
+            Tail.Y--;
+        }
+
+        if (Tail.Id == 9 && !_tail.Contains((Tail.X, Tail.Y)))
+            _tail.Add((Tail.X, Tail.Y));
+    }
+}
+
+class Knot
+{
+    public Knot(int id, int x, int y)
+    {
+        Id = id;
+        X = x;
+        Y = y;
+    }
+
+    public int Id { get; set; }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString() => $"{X},{Y}";
+}
