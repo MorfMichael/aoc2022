e8992ba1778af5e2cff7378571731bdd96e5ef58
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-12 20:37:26 +0100

    level12 part1

--- level12/Program.cs
+++ level12/Program.cs
@@ -8,8 +8,8 @@ for (int i = 0; i < lines.Length; i++)
 
     for (int j = 0; j < line.Length; j++)
     {
-        int height = line[j] - 'a';
-        if (line[j] == 'S') height = -1;
+        int height = line[j] - 'a' + 1;
+        if (line[j] == 'S') height = 1;
         if (line[j] == 'E') height = 26;
 
         points.Add(new Point(j, i, line[j], height, line[j] == 'S', line[j] == 'E'));
@@ -19,26 +19,28 @@ for (int i = 0; i < lines.Length; i++)
 var start = points.FirstOrDefault(x => x.start);
 var end = points.FirstOrDefault(x => x.end);
 
-List<(int x, int y, int height, int count)> steps = new();
-List<(int x, int y)> visited = new();
+List<(int x, int y, int height, int count)> steps = new() { (start.x, start.y, start.height, 0) };
+HashSet<(int x, int y)> visited = new() { };
 
-var cur = (start.x, start.y, height: -1, count: 0);
-while (true)
+var cur = steps.FirstOrDefault();
+
+while (steps.Any())
 {
     cur = steps.OrderBy(t => t.count).FirstOrDefault();
     steps.Remove(cur);
 
-    if (cur.x == end.x && cur.y == end.y) break;
-
     if (visited.Contains((cur.x, cur.y))) continue;
     visited.Add((cur.x, cur.y));
 
-    var next = new (int x, int y)[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
-    var ps = next.Select(t => points.FirstOrDefault(p => p.x == cur.x + t.x && p.y == cur.y + t.y && p.height <= cur.height + 1)).Where(t => t != null).OrderByDescending(t => t.height).ToList();
+    if (cur.x == end.x && cur.y == end.y) break;
 
-    foreach (var p in ps)
+    foreach (var next in new (int x, int y)[] { (-1, 0), (1, 0), (0, -1), (0, 1) })
     {
-        steps.Add((p.x, p.y, p.height, cur.count + 1));
+        var p = points.FirstOrDefault(p => p.x == cur.x + next.x && p.y == cur.y + next.y && p.height <= cur.height + 1);
+        if (p != null)
+        {
+            steps.Add((p.x, p.y, p.height, cur.count + 1));
+        }
     }
 }
 
