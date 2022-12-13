696cf1eb03ac7ac427ffc46ab4fb0fb6fc24b9d0
Michael Morf <michael.morf@kcc.at>
2022-12-13 14:47:02 +0100

    level13 paring

---
+++ level13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+List<(string[] Packets, int Index)> packets = File.ReadAllText("sample.in").Split("\r\n\r\n").Select((t, i) => (Packets: t.Split("\r\n"), Index: i + 1)).ToList();
+
+Console.WriteLine(packets.Count);
+
+
+string value = "[1,[2,[3,[4,[5,6,0]]]],8,9]";
+var split = value.Split(',');
+
+
+Stack<List<object>> start = new();
+List<object> current = null;
+foreach (var s in split)
+{
+    string edit = s;
+    while (edit.StartsWith("["))
+    {
+        if (current != null)
+            start.Push(current);
+        current = new List<object>();
+        edit = edit[1..];
+    }
+
+    if (int.TryParse(edit, out var integer)) current.Add(integer);
+    else
+    {
+        int i = 0;
+        string number = string.Empty;
+        while (edit.Any() && char.IsDigit(edit[0]))
+        {
+            number += edit[0];
+            edit = edit[1..];
+        }
+        if (number.Length > 0)
+            current.Add(int.Parse(number));
+
+        while (edit.Length > 0 && edit[0] == ']')
+        {
+            if (start.Any())
+            {
+                var prev = current;
+                current = start.Pop();
+                current.Add(prev);
+            }
+            edit = edit[1..];
+        }
+    }
+}
