b22dcbf97b7431ef714ff9f380241f4e89232493
Michael Morf <michael.morf@kcc.at>
2022-12-13 17:05:54 +0100

    level13 tryout

--- level13/Program.cs
+++ level13/Program.cs
@@ -1,48 +1,68 @@
 ﻿using System.Text;
 using System.Text.RegularExpressions;
 
-List<(string[] Packets, int Index)> packets = File.ReadAllText("sample.in").Split("\r\n\r\n").Select((t, i) => (Packets: t.Split("\r\n"), Index: i + 1)).ToList();
+List<(List<object> left, List<object> right, int Index)> packets = File.ReadAllText("sample.in").Split("\r\n\r\n").Select((t, i) => (Split: t.Split("\r\n"), Index: i + 1)).Select(t => (left: Parse(t.Split[0]), right: Parse(t.Split[1]), Index: t.Index)).ToList();
 
-Console.WriteLine(packets.Count);
-
-
-string value = "[1,[2,[3,[4,[5,6,0]]]],8,9]";
-
-Stack<List<object>> start = new();
-List<object> current = null;
-foreach (var s in value.Split(','))
+int count = 0;
+foreach (var packet in packets)
 {
-    string edit = s;
-    while (edit.StartsWith("["))
+    if (Compare(packet.left, packet.right)) count++;
+}
+
+bool Compare(List<object> left, List<object> right)
+{
+    for (int i = 0; i < left.Count; i++)
     {
-        if (current != null)
-            start.Push(current);
-        current = new List<object>();
-        edit = edit[1..];
+        if (i > right.Count) return false;
+
+        if (left[i].GetType() == typeof(int) && right[i].GetType() == typeof(int))
+        {
+        }
     }
 
-    if (int.TryParse(edit, out var integer)) current.Add(integer);
-    else
+    return true;
+}
+
+List<object> Parse(string input)
+{
+    Stack<List<object>> start = new();
+    List<object> current = null;
+    foreach (var s in input.Split(','))
     {
-        int i = 0;
-        string number = string.Empty;
-        while (edit.Any() && char.IsDigit(edit[0]))
+        string edit = s;
+        while (edit.StartsWith("["))
         {
-            number += edit[0];
+            if (current != null)
+                start.Push(current);
+            current = new List<object>();
             edit = edit[1..];
         }
-        if (number.Length > 0)
-            current.Add(int.Parse(number));
 
-        while (edit.Length > 0 && edit[0] == ']')
+        if (int.TryParse(edit, out var integer)) current.Add(integer);
+        else
         {
-            if (start.Any())
+            int i = 0;
+            string number = string.Empty;
+            while (edit.Any() && char.IsDigit(edit[0]))
             {
-                var prev = current;
-                current = start.Pop();
-                current.Add(prev);
+                number += edit[0];
+                edit = edit[1..];
             }
-            edit = edit[1..];
+            if (number.Length > 0)
+                current.Add(int.Parse(number));
+
+            while (edit.Length > 0 && edit[0] == ']')
+            {
+                if (start.Any())
+                {
+                    var prev = current;
+                    current = start.Pop();
+                    current.Add(prev);
+                }
+                edit = edit[1..];
+            }
         }
     }
+
+    return current;
 }
