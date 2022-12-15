d599ea5c22b9f1070765c544ad97c4858a89d1e9
Michael Morf <michael.morf@kcc.at>
2022-12-15 16:08:09 +0100

    level15 tryout

--- level15/Program.cs
+++ level15/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 string[] lines = File.ReadAllLines("level15.in");
 
-List<(int sx, int sy, int bx, int by)> scanners = new();
+List<(int sx, int sy, int bx, int by, int d)> scanners = new();
 HashSet<(int x, int y)> beacons = new();
 HashSet<(int x, int y)> not = new();
 
@@ -15,47 +16,44 @@ foreach (var line in lines)
     int sy = int.Parse(split[3][2..][..^1]);
     int bx = int.Parse(split[^2][2..][..^1]);
     int by = int.Parse(split[^1][2..]);
-
+    int d = ManhattanDistance(sx, sy, bx, by);
     beacons.Add((bx, by));
 
-    var s = (sx, sy, bx, by);
-    Console.WriteLine(s);
+    var s = (sx, sy, bx, by, d);
+    //Console.WriteLine(s);
     scanners.Add(s);
-    AddRange(sx, sy, bx, by);
-    //Print();
+    //AddRange(sx, sy, bx, by);
 }
 
-void Print()
+/*
+3,3
+4,4
+.......
+...#...
+..###..
+.##S##.
+..##B..
+...#...
+.......
+*/
+long bla = Range(2000000).ToList().Sum(x => x.x2 > x.x1 ? x.x2 - x.x1 : x.x1 - x.x2);
+Console.WriteLine(bla);
+
+IEnumerable<(int x1, int x2)> Range(int y)
 {
-    //for (int y = 0; y <= 40; y++)
+    foreach (var scanner in scanners)
     {
-        int y = 10;
-        for (int x = -10; x <= 30; x++)
-        {
-            if (scanners.Any(t => t.sx == x && t.sy == y)) Console.Write("S");
-            else if (scanners.Any(t => t.bx == x && t.by == y)) Console.Write("B");
-            else if (not.Contains((x, y))) Console.Write("#");
-            else Console.Write(".");
-        }
-        Console.WriteLine();
+        if (y > scanner.sy + scanner.d || y < scanner.sy - scanner.d) continue;
+        int myy = Math.Abs(scanner.sy - y);
+        var r = (x1: scanner.sx - (scanner.d - myy), x2: scanner.sx + (scanner.d - myy));
+        Console.WriteLine(scanner + "; " + r + " -> " + (r.x2 > r.x1 ? r.x2 - r.x1 : r.x1 - r.x2));
+        yield return r;
     }
 }
-
-Console.WriteLine(not.Count(n => n.y == check));
 
 void AddRange(int sx, int sy, int bx, int by)
 {
     int value = ManhattanDistance(sx, sy, bx, by);
-    for (int y = -value; y <= value; y++)
-    {
-        if (sy+y != check) continue;
-        for (int x = -value+Math.Abs(y); x <= value-Math.Abs(y); x++)
-        {
-            var n = (sx + x, sy + y);
-            if (beacons.Contains(n)) continue;
-            not.Add(n);
-        }
-    }
 }
 
 static int ManhattanDistance(int x1, int y1, int x2, int y2) => Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
