c4ef3807809b4bc44c7b56ca555a5e8d24fc26c6
Michael Morf <michael.morf@kcc.at>
2022-12-21 16:31:01 +0100

    level20 part2

--- level20/Program.cs
+++ level20/Program.cs
@@ -10,45 +10,46 @@ for (int i = 0; i < lines.Length; i++)
 
     if (string.IsNullOrWhiteSpace(line)) continue;
 
-    long value = long.Parse(line);// * 811589153;
+    long value = long.Parse(line) * 811589153;
 
     var entry = new Entry(value);
     entries.Add(entry);
     move.Add(entry);
 }
 
-Console.WriteLine(string.Join(",", move));
+//Console.WriteLine(string.Join(",", move));
 
-//for (int i = 0; i < 10; i++)
-//{
-foreach (var entry in entries)
+for (int i = 0; i < 10; i++)
 {
-    int curIdx = move.IndexOf(entry);
-    long newIdx = curIdx + entry.Value;
+    foreach (var entry in entries)
+    {
+        int curIdx = move.IndexOf(entry);
+        long newIdx = curIdx + entry.Value;
 
-    if (newIdx >= lines.Length) newIdx = (newIdx % lines.Length) + 1;
-    if (newIdx < 0) newIdx = (lines.Length-1) - Math.Abs(newIdx) % lines.Length;
+        bool left = newIdx < 0;
+        newIdx = Math.Abs(newIdx) % (lines.Length - 1);
 
-    if (newIdx == 0) newIdx = lines.Length - 1;
+        if (left) newIdx = lines.Length - 1 - newIdx;
+        if (newIdx == 0) newIdx = lines.Length - 1;
 
-    //Console.WriteLine($"{curIdx} + {entry.Value} = {curIdx + entry.Value} -> {newIdx}");
+        //Console.WriteLine($"{curIdx} + {entry.Value} = {curIdx + entry.Value} -> {newIdx}");
 
-    move.Remove(entry);
-    move.Insert((int)newIdx, entry);
+        move.Remove(entry);
+        move.Insert((int)newIdx, entry);
 
-    //Console.WriteLine(string.Join(",", move.Select(t => t.Value)));
-    //Console.ReadKey();
+        //Console.WriteLine(string.Join(",", move.Select(t => t.Value)));
+        //Console.ReadKey();
+    }
 }
-//}
 
 var zero = move.FirstOrDefault(t => t.IsZero);
 int a = (move.IndexOf(zero) + 1000) % lines.Length;
 int b = (move.IndexOf(zero) + 2000) % lines.Length;
 int c = (move.IndexOf(zero) + 3000) % lines.Length;
 
-Console.WriteLine(a);
-Console.WriteLine(b);
-Console.WriteLine(c);
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
 
 Console.WriteLine(move[a]);
 Console.WriteLine(move[b]);
