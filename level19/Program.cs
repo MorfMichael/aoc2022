b9cc8d361e8af8a0cab9853b94cfc595759735fb
Ing. Morf Michael <michael.morf@gmx.at>
2022-12-31 14:13:47 +0100

    level19 part1

---
+++ level19/Program.cs
@@ -0,0 +1,128 @@
+﻿string[] lines = File.ReadAllLines("level19.in");
+
+int count = 0;
+
+string[] resources = new string[] { "ore", "clay", "obsidian", "geode" };
+
+Dictionary<int, int[][]> blueprints = new();
+
+for (int i = 0; i < lines.Length; i++)
+{
+    string line = lines[i];
+
+    if (string.IsNullOrWhiteSpace(line)) continue;
+
+    var bp = GetCosts(line);
+
+    var v = Calculate(bp.bots, bp.maxspend, new(), 24, new int[] { 1, 0, 0, 0 }, new int[] { 0, 0, 0, 0 });
+
+    count += (i + 1) * v;
+}
+
+Console.WriteLine(count);
+
+
+((int ramt, int rtype)[][] bots, int[] maxspend) GetCosts(string line)
+{
+    var split = line.Split(new char[] { ':', '.' }, StringSplitOptions.RemoveEmptyEntries);
+    var blueprint = split[0].Split();
+    int id = int.Parse(blueprint[1]);
+
+    (int ramt, int rtype)[][] bots = new (int ramt, int rtype)[4][];
+    int[] maxspend = new int[3] { 0, 0, 0 };
+
+    string[] bsplit = split[1..];
+    for (int b = 0; b < bsplit.Length; b++)
+    {
+        string r = bsplit[b];
+        var rsplit = r.Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(t => t != "and").ToArray();
+
+        string type = rsplit[1];
+        var costs = rsplit[4..].Chunk(2).Select(t => (ramt: int.Parse(t[0]), rtype: Array.IndexOf(resources, t[1]))).ToArray();
+        bots[b] = costs;
+        foreach (var c in costs)
+        {
+            maxspend[c.rtype] = Math.Max(maxspend[c.rtype], c.ramt);
+        }
+    }
+
+    return (bots, maxspend);
+}
+
+int Calculate((int ramt, int rtype)[][] bp, int[] maxspend, Dictionary<string, int> cache, int time, int[] bots, int[] amt)
+{
+    if (time == 0)
+        return amt[3];
+
+    var key = $"{time};{string.Join(",", bots)};{string.Join(",", amt)}";
+    if (cache.ContainsKey(key)) return cache[key];
+
+    int max = amt[3] + bots[3] * time;
+
+    for (int i = 0; i < bp.Length; i++) // recipes
+    {
+        if (i != 3 && bots[i] >= maxspend[i])
+            continue;
+
+        int wait = 0;
+        bool br = false;
+
+        foreach (var res in bp[i])
+        {
+            if (bots[res.rtype] == 0)
+            {
+                br = true;
+                break;
+            }
+
+            wait = Math.Max(wait, (int)Math.Ceiling((double)(res.ramt - amt[res.rtype]) / bots[res.rtype]));
+        }
+
+        if (!br)
+        {
+            var remtime = time - wait - 1;
+            if (remtime <= 0) continue;
+
+            var nbots = bots.ToArray();
+            var namt = amt.Zip(bots).Select(zip => zip.First + zip.Second * (wait + 1)).ToArray();
+
+            foreach (var res in bp[i])
+            {
+                namt[res.rtype] -= res.ramt;
+            }
+            nbots[i] += 1;
+
+            for (int j = 0; j < 3; j++)
+            {
+                namt[j] = Math.Min(namt[j], maxspend[j] * remtime);
+            }
+
+            int x = Calculate(bp, maxspend, cache, remtime, nbots, namt);
+            if (x > max)
+            {
+                //Console.WriteLine(string.Join(",", nbots));
+                max = x;
+            }
+        }
+    }
+
+    cache.Add(key, max);
+    return max;
+}
+
+
+int NameToIndex(string name) => name switch
+{
+    "ore" => 0,
+    "clay" => 1,
+    "obsidian" => 2,
+    "geode" => 3,
+};
+
+string IndexToName(int index) => index switch
+{
+    0 => "ore",
+    1 => "clay",
+    2 => "obsidian",
+    3 => "geode",
+};
--- level19/_Program.cs
+++ level19/_Program.cs
@@ -1,176 +1,176 @@
-﻿string[] lines = File.ReadAllLines("level19.in");
+﻿//string[] lines = File.ReadAllLines("sample.in");
 
-int count = 0;
+//int count = 0;
 
-List<Blueprint> blueprints = new();
+//List<Blueprint> blueprints = new();
 
-for (int i = 0; i < lines.Length; i++)
-{
-    string line = lines[i];
+//for (int i = 0; i < lines.Length; i++)
+//{
+//    string line = lines[i];
 
-    if (string.IsNullOrWhiteSpace(line)) continue;
+//    if (string.IsNullOrWhiteSpace(line)) continue;
 
-    blueprints.Add(new Blueprint(line));
+//    blueprints.Add(new Blueprint(line));
 
-    count++;
-}
+//    count++;
+//}
 
-Console.WriteLine(count);
+//Console.WriteLine(count);
 
-Dictionary<string, int> init = new()
-{
-    { "ore", 0 },
-    { "clay", 0 },
-    { "obsidian", 0 },
-    { "geode", 0 },
-};
+//Dictionary<string, int> init = new()
+//{
+//    { "ore", 0 },
+//    { "clay", 0 },
+//    { "obsidian", 0 },
+//    { "geode", 0 },
+//};
 
-foreach (var bp in blueprints)
-{
-    HashSet<(int minute, string collecting, string resources)> seen = new();
+//foreach (var bp in blueprints)
+//{
+//    HashSet<(int minute, string collecting, string resources)> seen = new();
 
-    Queue<(int minute, List<Robot> collecting, Dictionary<string, int> resources)> queue = new();
-    queue.Enqueue((24, new() { new Robot("ore", new()) }, CopyResources(init)));
+//    Queue<(int minute, List<Robot> collecting, Dictionary<string, int> resources)> queue = new();
+//    queue.Enqueue((24, new() { new Robot("ore", new()) }, CopyResources(init)));
 
-    long i = 0;
-    while (queue.Any())
-    {
-        var cur = queue.Dequeue();
+//    long i = 0;
+//    while (queue.Any())
+//    {
+//        var cur = queue.Dequeue();
 
-        var s = (cur.minute, string.Join(",", cur.collecting), string.Join(",", cur.resources));
+//        var s = (cur.minute, string.Join(",", cur.collecting), string.Join(",", cur.resources));
 
-        if (seen.Contains(s))
-            continue;
-        
-        seen.Add(s);
+//        if (seen.Contains(s))
+//            continue;
 
-        if (i % 10000 == 0)
-            Console.WriteLine((bp.Id, bp.Geode, seen.Count));
+//        seen.Add(s);
 
-        if (cur.minute <= 0)
-        {
-            //Console.WriteLine(string.Join(", ", cur.collecting.GroupBy(t => t.Type).Select(t => $"{t.Count()} {t.Key}")));
-            //Console.WriteLine($"Blueprint {bp.Id}: {geode}");
-            //Console.WriteLine(string.Join(",", cur.resources));
-            bp.Geode = Math.Max(bp.Geode, cur.resources["geode"]);
-            continue;
-        }
+//        if (i % 10000 == 0)
+//            Console.WriteLine((bp.Id, bp.Geode, seen.Count));
 
-        //if (cur.resources["ore"] >= cur.minute * bp.Robots.Max(t => t.Cost["ore"]))
-        //{
-        //    cur.resources["ore"] = cur.minute * bp.Robots.Max(t => t.Cost["ore"]);
-        //}
+//        if (cur.minute <= 0)
+//        {
+//            //Console.WriteLine(string.Join(", ", cur.collecting.GroupBy(t => t.Type).Select(t => $"{t.Count()} {t.Key}")));
+//            //Console.WriteLine($"Blueprint {bp.Id}: {geode}");
+//            //Console.WriteLine(string.Join(",", cur.resources));
+//            bp.Geode = Math.Max(bp.Geode, cur.resources["geode"]);
+//            continue;
+//        }
 
-        //if (cur.resources["clay"] >= cur.minute * bp.Robots[2].Cost["clay"])
-        //    cur.resources["clay"] = cur.minute * bp.Robots[2].Cost["clay"];
+//        //if (cur.resources["ore"] >= cur.minute * bp.Robots.Max(t => t.Cost["ore"]))
+//        //{
+//        //    cur.resources["ore"] = cur.minute * bp.Robots.Max(t => t.Cost["ore"]);
+//        //}
 
-        //if (cur.resources["obsidian"] >= cur.minute * bp.Robots[3].Cost["obsidian"])
-        //    cur.resources["obsidian"] = cur.minute * bp.Robots[3].Cost["obsidian"];
+//        //if (cur.resources["clay"] >= cur.minute * bp.Robots[2].Cost["clay"])
+//        //    cur.resources["clay"] = cur.minute * bp.Robots[2].Cost["clay"];
 
-        var options = bp.Robots.Where(t => t.CanSpend(cur.resources)).ToList();
+//        //if (cur.resources["obsidian"] >= cur.minute * bp.Robots[3].Cost["obsidian"])
+//        //    cur.resources["obsidian"] = cur.minute * bp.Robots[3].Cost["obsidian"];
 
-        // collect
-        foreach (var r in cur.collecting)
-        {
-            cur.resources[r.Type]++;
-        }
-        
-        queue.Enqueue((cur.minute - 1, cur.collecting, cur.resources));
-        // add options to queue
-        foreach (var option in options)
-        {
-            var newcollecting = new List<Robot>(cur.collecting);
-            newcollecting.Add(option);
-            var newresources = CopyResources(cur.resources);
-            option.Spend(newresources);
-            queue.Enqueue((cur.minute - 1, newcollecting, newresources));
-        }
-        i++;
-    }
+//        var options = bp.Robots.Where(t => t.CanSpend(cur.resources)).ToList();
 
-    Console.WriteLine($"Blueprint {bp.Id}: {bp.Geode}");
-}
+//        // collect
+//        foreach (var r in cur.collecting)
+//        {
+//            cur.resources[r.Type]++;
+//        }
 
+//        queue.Enqueue((cur.minute - 1, cur.collecting, cur.resources));
+//        // add options to queue
+//        foreach (var option in options)
+//        {
+//            var newcollecting = new List<Robot>(cur.collecting);
+//            newcollecting.Add(option);
+//            var newresources = CopyResources(cur.resources);
+//            option.Spend(newresources);
+//            queue.Enqueue((cur.minute - 1, newcollecting, newresources));
+//        }
+//        i++;
+//    }
 
-Console.WriteLine(blueprints.Sum(x => x.Id * x.Geode));
+//    Console.WriteLine($"Blueprint {bp.Id}: {bp.Geode}");
+//}
 
 
-Dictionary<string, int> CopyResources(Dictionary<string, int> resources) => resources.ToDictionary(t => t.Key, t => t.Value);
-string PrintRobots(IEnumerable<Robot> robots) => string.Join("|", robots.GroupBy(t => t.Type).Select(t => $"{t.Count()} {t.Key}"));
+//Console.WriteLine(blueprints.Sum(x => x.Id * x.Geode));
 
-class Blueprint
-{
-    public Blueprint(string line)
-    {
-        var split = line.Split(new char[] { ':', '.' }, StringSplitOptions.RemoveEmptyEntries);
-        var blueprint = split[0].Split();
-        Id = int.Parse(blueprint[1]);
-        Robots = new List<Robot>();
-        foreach (var r in split[1..])
-        {
-            var rsplit = r.Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(t => t != "and").ToArray();
 
-            string type = rsplit[1];
-            var costs = rsplit[4..].Chunk(2).ToDictionary(x => x[1], x => int.Parse(x[0]));
+//Dictionary<string, int> CopyResources(Dictionary<string, int> resources) => resources.ToDictionary(t => t.Key, t => t.Value);
+//string PrintRobots(IEnumerable<Robot> robots) => string.Join("|", robots.GroupBy(t => t.Type).Select(t => $"{t.Count()} {t.Key}"));
 
-            Robots.Add(new Robot(type, costs));
-        }
-    }
+//class Blueprint
+//{
+//    public Blueprint(string line)
+//    {
+//        var split = line.Split(new char[] { ':', '.' }, StringSplitOptions.RemoveEmptyEntries);
+//        var blueprint = split[0].Split();
+//        Id = int.Parse(blueprint[1]);
+//        Robots = new List<Robot>();
+//        foreach (var r in split[1..])
+//        {
+//            var rsplit = r.Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(t => t != "and").ToArray();
 
-    public int Id { get; set; }
-    public List<Robot> Robots { get; set; }
+//            string type = rsplit[1];
+//            var costs = rsplit[4..].Chunk(2).ToDictionary(x => x[1], x => int.Parse(x[0]));
 
-    public int Geode { get; set; }
-}
+//            Robots.Add(new Robot(type, costs));
+//        }
+//    }
 
-class Robot
-{
-    public Robot(string type, Dictionary<string, int> cost)
-    {
-        Type = type;
-        Cost = cost;
-    }
+//    public int Id { get; set; }
+//    public List<Robot> Robots { get; set; }
 
-    public string Type { get; set; }
+//    public int Geode { get; set; }
+//}
 
-    public int Weight => Type switch
-    {
-        "ore" => 0,
-        "clay" => 1,
-        "obsidian" => 2,
-        "geode" => 3,
-    };
+//class Robot
+//{
+//    public Robot(string type, Dictionary<string, int> cost)
+//    {
+//        Type = type;
+//        Cost = cost;
+//    }
 
-    public Dictionary<string, int> Cost { get; set; }
+//    public string Type { get; set; }
 
-    public bool CanSpend(Dictionary<string, int> resources) => Cost.All(c => resources[c.Key] >= c.Value);
+//    public int Weight => Type switch
+//    {
+//        "ore" => 0,
+//        "clay" => 1,
+//        "obsidian" => 2,
+//        "geode" => 3,
+//    };
 
-    public void Spend(Dictionary<string, int> resources)
-    {
-        foreach (var c in Cost)
-        {
-            resources[c.Key] -= c.Value;
-        }
-    }
+//    public Dictionary<string, int> Cost { get; set; }
 
-    public int Takes(Dictionary<string, int> resources, List<Robot> collecting)
-    {
-        if (Cost.All(c => resources[c.Key] >= c.Value))
-        {
-            return 0;
-        }
+//    public bool CanSpend(Dictionary<string, int> resources) => Cost.All(c => resources[c.Key] >= c.Value);
 
-        bool possible = Cost.All(c => collecting.Any(r => r.Type == c.Key));
-        if (possible)
-        {
-            int minute = Cost.Select(c => (c.Key, Remaining: c.Value - resources[c.Key])).Max(t => t.Remaining / collecting.Count(x => x.Type == t.Key));
-            return minute;
-        }
+//    public void Spend(Dictionary<string, int> resources)
+//    {
+//        foreach (var c in Cost)
+//        {
+//            resources[c.Key] -= c.Value;
+//        }
+//    }
 
-        return -1;
-    }
+//    public int Takes(Dictionary<string, int> resources, List<Robot> collecting)
+//    {
+//        if (Cost.All(c => resources[c.Key] >= c.Value))
+//        {
+//            return 0;
+//        }
 
-    public override string ToString() => Type;
-}
+//        bool possible = Cost.All(c => collecting.Any(r => r.Type == c.Key));
+//        if (possible)
+//        {
+//            int minute = Cost.Select(c => (c.Key, Remaining: c.Value - resources[c.Key])).Max(t => t.Remaining / collecting.Count(x => x.Type == t.Key));
+//            return minute;
+//        }
+
+//        return -1;
+//    }
+
+//    public override string ToString() => Type;
+//}
 
 
